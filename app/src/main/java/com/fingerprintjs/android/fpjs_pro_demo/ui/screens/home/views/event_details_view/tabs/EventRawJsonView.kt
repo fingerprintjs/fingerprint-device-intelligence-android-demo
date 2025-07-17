7a174367e7e4013d937093d2c3eeade888dcefd8
Bryan Logan <bryan.logan@fingerprint.com>
2025-07-17 11:51:39 -0500

    Do highlighting

--- app/src/main/java/com/fingerprintjs/android/fpjs_pro_demo/ui/screens/home/views/event_details_view/tabs/EventRawJsonView.kt
+++ app/src/main/java/com/fingerprintjs/android/fpjs_pro_demo/ui/screens/home/views/event_details_view/tabs/EventRawJsonView.kt
@@ -15,9 +15,13 @@ import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.fingerprintjs.android.fpjs_pro_demo.ui.screens.home.viewmodel.HomeScreenUiState
+import com.fingerprintjs.android.fpjs_pro_demo.ui.theme.AppColors
 import com.fingerprintjs.android.fpjs_pro_demo.ui.theme.AppTheme
 import com.fingerprintjs.android.fpjs_pro_demo.utils.PreviewMultipleConfigurations
 import com.fingerprintjs.android.fpjs_pro_demo.utils.ShowPreview
@@ -27,6 +31,7 @@ fun EventRawJsonView(
     modifier: Modifier,
     code: String,
 ) {
+    val spannedCode = addSpan(code)
     Column(modifier = modifier.padding(start = 16.dp)) {
         Spacer(
             modifier = Modifier
@@ -55,7 +60,7 @@ fun EventRawJsonView(
                         color = AppTheme.materialTheme.colorScheme.onSurfaceVariant,
                         style = AppTheme.extendedTheme.typography.codeNormal,
                         softWrap = false,
-                        text = code,
+                        text = spannedCode,
                     )
                 }
                 Spacer(modifier = Modifier.width(16.dp))
@@ -70,6 +75,38 @@ fun EventRawJsonView(
     }
 }
 
+private fun addSpan(s: String): AnnotatedString  {
+    var colorString = AnnotatedString("")
+    val startOfObjectOrArray = Regex("^\\s*[{\\[].*")
+    s.lines().forEach {
+        val splits = it.split("\":", limit = 2)
+        colorString += AnnotatedString(splits[0])
+
+        if (it.contains("subdivision")) {
+            if (it.contains("asjkldfklasdfjkl")) {
+                return colorString
+            }
+        }
+
+        if (splits.size > 1) {
+            colorString += AnnotatedString("\":")
+            val part2 = splits[1]
+            if (part2.matches(startOfObjectOrArray)) {
+                colorString += AnnotatedString(part2)
+            } else {
+                colorString += AnnotatedString(part2.removeSuffix(","), SpanStyle(
+                    color = AppColors.Orange400,
+                    fontWeight = FontWeight.ExtraBold
+                ))
+                if (part2.endsWith(',')) {
+                    colorString += AnnotatedString(",")
+                }
+            }
+        }
+        colorString += AnnotatedString("\n")
+    }
+    return colorString
+}
 private fun lineNumbersString(from: String): String =
     (1..from.lines().count())
         .joinToString(separator = "\n") { it.toString() }
