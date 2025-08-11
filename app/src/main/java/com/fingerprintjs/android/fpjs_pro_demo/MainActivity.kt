3a94e1c64d3107e929ff277fd1d93f663000925b
Bryan Logan <bryan.logan@fingerprint.com>
2025-08-11 11:56:31 -0500

    Location permissions

--- app/src/main/java/com/fingerprintjs/android/fpjs_pro_demo/MainActivity.kt
+++ app/src/main/java/com/fingerprintjs/android/fpjs_pro_demo/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.fingerprintjs.android.fpjs_pro_demo
 
+import android.Manifest
 import android.animation.ObjectAnimator
+import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
 import android.view.View
 import android.view.animation.AccelerateInterpolator
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.animation.doOnEnd
+import androidx.core.app.ActivityCompat
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.fingerprintjs.android.fpjs_pro_demo.ui.navigation.NavScreen
 import com.fingerprintjs.android.fpjs_pro_demo.ui.theme.AppTheme
@@ -37,5 +41,36 @@ class MainActivity : ComponentActivity() {
                 NavScreen()
             }
         }
+
+        checkLocationPermissions()
+    }
+
+    fun checkLocationPermissions() {
+        if ((ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION) ==
+                    PackageManager.PERMISSION_GRANTED) ||
+            (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION) ==
+                    PackageManager.PERMISSION_GRANTED)
+        ) {
+            // We have at least some location permission, so we can proceed
+            return
+        }
+
+        val locationPermissionRequest = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) { permissions ->
+            // Whether they give us permission or not, we can now make the Fingerprint call
+        }
+
+        // Request permission
+        locationPermissionRequest.launch(
+            arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+        )
     }
 }
