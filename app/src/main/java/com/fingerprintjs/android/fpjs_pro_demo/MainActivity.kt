673669eb74a547eb0608be6d2c606ad5c64a7b64
Bryan Logan <bryan.logan@fingerprint.com>
2025-08-13 11:11:23 -0500

    Detekt fixes

--- app/src/main/java/com/fingerprintjs/android/fpjs_pro_demo/MainActivity.kt
+++ app/src/main/java/com/fingerprintjs/android/fpjs_pro_demo/MainActivity.kt
@@ -46,14 +46,18 @@ class MainActivity : ComponentActivity() {
     }
 
     fun checkLocationPermissions() {
-        if ((ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_FINE_LOCATION) ==
-                    PackageManager.PERMISSION_GRANTED) ||
-            (ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_COARSE_LOCATION) ==
-                    PackageManager.PERMISSION_GRANTED)
+        if ((
+                ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED
+                ) ||
+            (
+                ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED
+                )
         ) {
             // We have at least some location permission, so we can proceed
             return
