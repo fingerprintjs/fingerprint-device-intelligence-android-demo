5ac06df244912c493d357b1ebe68775e2f5c517e
Alex Z <alex.zaitsev@fingerprint.com>
2025-08-26 10:33:29 -0600

    MOBILE-748: Update default timeout

--- app/src/main/java/com/fingerprintjs/android/fpjs_pro_demo/domain/identification/IdentificationProvider.kt
+++ app/src/main/java/com/fingerprintjs/android/fpjs_pro_demo/domain/identification/IdentificationProvider.kt
@@ -34,7 +34,6 @@ class IdentificationProvider @Inject constructor(
                     apiKey = if (it.enabled) it.public else Credentials.apiKey,
                     endpointUrl = if (it.enabled) it.region.endpointUrl else Credentials.endpointUrl,
                     extendedResponseFormat = true,
-                    locationTimeoutMillis = 30_000L,
                     allowUseOfLocationData = true,
                 )
             )
