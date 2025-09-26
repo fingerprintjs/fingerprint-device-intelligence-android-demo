f8d53602df5783a3dc99c78f4ab638733c4a60ae
Bryan Logan <bryan.logan@fingerprint.com>
2025-09-26 08:56:04 -0500

    Update TypographyExtended.kt

--- app/src/main/java/com/fingerprintjs/android/fpjs_pro_demo/ui/theme/TypographyExtended.kt
+++ app/src/main/java/com/fingerprintjs/android/fpjs_pro_demo/ui/theme/TypographyExtended.kt
@@ -1,7 +1,6 @@
 package com.fingerprintjs.android.fpjs_pro_demo.ui.theme
 
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.DeviceFontFamilyName
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
