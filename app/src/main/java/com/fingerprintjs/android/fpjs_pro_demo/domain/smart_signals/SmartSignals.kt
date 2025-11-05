946053c91b3224672f53ad22eeb600dc2a65bc70
manjunathnijaguna-fp <manjunath.nijaguna@fingerprint.com>
2025-11-05 11:36:25 +0530

    Mobile-766 : asn proxy ip blocking addition (#53)

--- app/src/main/java/com/fingerprintjs/android/fpjs_pro_demo/domain/smart_signals/SmartSignals.kt
+++ app/src/main/java/com/fingerprintjs/android/fpjs_pro_demo/domain/smart_signals/SmartSignals.kt
@@ -3,6 +3,7 @@ package com.fingerprintjs.android.fpjs_pro_demo.domain.smart_signals
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.JsonElement
 
+@Suppress("LongParameterList")
 class SmartSignals(
     val clonedApp: SmartSignalInfo<SmartSignal.ClonedApp>,
     val emulator: SmartSignalInfo<SmartSignal.Emulator>,
@@ -14,6 +15,9 @@ class SmartSignals(
     val vpn: SmartSignalInfo<SmartSignal.Vpn>,
     val tampering: SmartSignalInfo<SmartSignal.Tampering>,
     val mitm: SmartSignalInfo<SmartSignal.Mitm>,
+    val ipBlocklist: SmartSignalInfo<SmartSignal.IPBlocklist>,
+    val proxy: SmartSignalInfo<SmartSignal.Proxy>,
+    val ipInfo: SmartSignalInfo<SmartSignal.IPInfo>
 )
 
 sealed class SmartSignalInfo<out T : SmartSignal>(val rawKey: String) {
@@ -84,7 +88,7 @@ sealed class SmartSignal {
         val result: Boolean,
         val originTimezone: String? = null,
         val originCountry: String? = null,
-        val methods: Map<String, Boolean> = mapOf()
+        val methods: Map<String, Boolean> = mapOf(),
     ) : SmartSignal()
 
     @Serializable
@@ -96,5 +100,73 @@ sealed class SmartSignal {
     @Serializable
     data class Mitm(
         val result: Boolean,
+    ) : SmartSignal()
+
+    @Serializable
+    data class ASN(
+        val asn: String,
+        val name: String,
+        val network: String,
+    ) : SmartSignal()
+
+    @Serializable
+    data class DataCenter(
+        val result: Boolean,
+        val name: String,
+    ) : SmartSignal()
+
+    @Serializable
+    data class IPBlocklist(
+        val result: Boolean,
+        val details: Map<String, Boolean> = mapOf(),
+    ) : SmartSignal()
+
+    @Serializable
+    data class Proxy(
+        val result: Boolean,
+        val confidence: String,
+        val details: Map<String, String> = mapOf(),
+    ) : SmartSignal()
+
+    @Serializable
+    data class IPInfo(
+        val v4: IPV4,
+    ) : SmartSignal()
+
+    @Serializable
+    data class GeoLocation(
+        val accuracyRadius: Int,
+        val latitude: Double,
+        val longitude: Double,
+        val postalCode: Int,
+        val timezone: String,
+        val city: City,
+        val country: Country,
+        val continent: Continent,
+    ) : SmartSignal()
+
+    @Serializable
+    data class City(
+        val name: String,
+    )
+
+    @Serializable
+    data class Country(
+        val code: String,
+        val name: String,
+    )
+
+    @Serializable
+    data class Continent(
+        val code: String,
+        val name: String,
+    )
+
+    @Serializable
+    data class IPV4(
+        val address: String,
+        val geolocation: GeoLocation,
+        val asn: ASN,
+        val datacenter: DataCenter,
     ) : SmartSignal()
 }
