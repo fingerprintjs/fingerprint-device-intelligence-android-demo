dbe47c9e3a91a2b05ff9fca87fede3b361838521
Manjunath Nijaguna <manjunath.nijaguna@fingerprint.com>
2026-02-24 15:27:52 +0530

    MOBILE-1063: Version bump for release v3.8.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,9 +10,9 @@ val local = Properties().apply {
 }
 
 @Suppress("PropertyName")
-val VERSION_NAME= project.findProperty("VERSION_NAME")?.toString() ?: "3.8.2"
+val VERSION_NAME= project.findProperty("VERSION_NAME")?.toString() ?: "3.8.3"
 @Suppress("PropertyName")
-val VERSION_CODE= project.findProperty("VERSION_CODE")?.toString()?.toInt() ?: 45
+val VERSION_CODE= project.findProperty("VERSION_CODE")?.toString()?.toInt() ?: 46
 val useFpProDebugVersion =
     false // switch to true when needed to debug the locally built library
 val fingerprintProLib = if (useFpProDebugVersion) libs.fingerprint.pro.debug else libs.fingerprint.pro.asProvider()
