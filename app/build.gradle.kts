a200ff04592e5f615f0bd939e59302924bd9cc6c
rajabhargava21 <raja.bhargava@fingerprint.com>
2026-03-19 23:02:07 +0530

    MOBILE-1112: updated sdk and app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,9 +10,9 @@ val local = Properties().apply {
 }
 
 @Suppress("PropertyName")
-val VERSION_NAME= project.findProperty("VERSION_NAME")?.toString() ?: "3.9.0"
+val VERSION_NAME= project.findProperty("VERSION_NAME")?.toString() ?: "3.9.1"
 @Suppress("PropertyName")
-val VERSION_CODE= project.findProperty("VERSION_CODE")?.toString()?.toInt() ?: 53
+val VERSION_CODE= project.findProperty("VERSION_CODE")?.toString()?.toInt() ?: 54
 val useFpProDebugVersion =
     false // switch to true when needed to debug the locally built library
 val fingerprintProLib = if (useFpProDebugVersion) libs.fingerprint.pro.debug else libs.fingerprint.pro.asProvider()
