25707926576b20b2550838d2d8e6d9706d67108f
rajabhargava21 <raja.bhargava@fingerprint.com>
2026-08-31 18:10:57 +0530

    MOBILE-1450: version bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,9 +11,9 @@ val local = Properties().apply {
 }
 
 @Suppress("PropertyName")
-val VERSION_NAME= project.findProperty("VERSION_NAME")?.toString() ?: "4.1.5"
+val VERSION_NAME= project.findProperty("VERSION_NAME")?.toString() ?: "4.2.0"
 @Suppress("PropertyName")
-val VERSION_CODE= project.findProperty("VERSION_CODE")?.toString()?.toInt() ?: 67
+val VERSION_CODE= project.findProperty("VERSION_CODE")?.toString()?.toInt() ?: 68
 val useFpProDebugVersion =
     false // switch to true when needed to debug the locally built library
 val fingerprintProLib = if (useFpProDebugVersion) libs.fingerprint.pro.debug else libs.fingerprint.pro.asProvider()
